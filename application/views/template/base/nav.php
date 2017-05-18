f7bae78fdb4fe760ea3605a263aa6d6148f95136
amelia <yu.a@live.com>
2017-05-18 10:09:34 +0200

    creation navbar

---
+++ application/views/template/base/nav.php
@@ -0,0 +1,28 @@
+ <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse navi">
+            <div class="container">
+                <div class="col-md-4">
+                    <h3>Jeremy</h3>
+                </div>
+                <div class="col-md-7 col-md-offset-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a class="links" href="<?=base_url('#accueil');?>">Home</a></li>
+                        <li><a class="links" href="<?=base_url('#heroes');?>">Portfolio</a></li>
+                        <li><a class="links" href="<?=base_url('#appli');?>">About</a></li>
+                        <li><a class="links" href="<?=base_url('#contact');?>">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
