7d38362d63da576f0b5a948a7ba979a7f2d614c1
amelia <yu.a@live.com>
2017-05-18 11:16:21 +0200

    content home

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,10 +1,28 @@
-
-<div class="jumbotron">
-    <div class="container">
+<div class="container">
+    <div class="jumbotron">
         <h1>Hello, world!</h1>
         <p>Contents ...</p>
         <p>
-            <a class="btn btn-primary btn-lg">Learn more</a>
+             <a class="btn btn-primary btn-lg">Learn more</a>
         </p>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img class="img-responsive" src="../../../assets/img/1.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos nam temporibus sit accusantium ullam voluptate magni, magnam praesentium, reprehenderit architecto doloribus officiis aperiam itaque veritatis pariatur ab, nisi iusto!</p>
+        </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img class="img-responsive" src="../../../assets/img/2.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos nam temporibus sit accusantium ullam voluptate magni, magnam praesentium, reprehenderit architecto doloribus officiis aperiam itaque veritatis pariatur ab, nisi iusto!</p>
+        </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img class="img-responsive" src="../../../assets/img/3.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos nam temporibus sit accusantium ullam voluptate magni, magnam praesentium, reprehenderit architecto doloribus officiis aperiam itaque veritatis pariatur ab, nisi iusto!</p>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <img class="img-responsive" src="../../../assets/img/4.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos nam temporibus sit accusantium ullam voluptate magni, magnam praesentium, reprehenderit architecto doloribus officiis aperiam itaque veritatis pariatur ab, nisi iusto!</p>
+        </div>
     </div>
 </div>
