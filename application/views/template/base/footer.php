f1fda91f706ab81dab734dc0710cdfb0772c66f4
amelia <yu.a@live.com>
2017-05-18 11:16:00 +0200

    creation footer de base

---
+++ application/views/template/base/footer.php
@@ -0,0 +1,22 @@
+<div class="bg-footer">
+    <div class="container">
+        <div class="col-md-5">
+            <h2>Footer</h2>
+        </div>
+        <div class="col-md-4 col-md-offset-3 icons">
+            <div class="col-md-3">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-flickr" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+
+</div>
